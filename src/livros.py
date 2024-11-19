9a50903342914f296b630e5ece9c45dce548f5de
Eduardo Ribeiro <eduardorb2842@gmail.com>
2024-11-19 15:34:02 +0000

    Funcao para adicionar livros

--- src/livros.py
+++ src/livros.py
@@ -1,1 +1,13 @@
-#TODO
+from src.db_connection import get_connection
+
+def adicionar_livro():
+    titulo = input("Título: ")
+    autor = input("Autor: ")
+    ano = int(input("Ano: "))
+    
+    conexao = get_connection()
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO livros (titulo, autor, ano) VALUES (?, ?, ?)", (titulo, autor, ano))
+    conexao.commit()
+    conexao.close()
+    print("Livro adicionado com sucesso!")
