12b174234f5f0e09602710cb00e8804159952851
Pedro Brandao <pedrowubrandao@gmail.com>
2024-11-19 14:37:45 +0000

    Adicionei função de adicionar leitor

--- src/leitores.py
+++ src/leitores.py
@@ -1,1 +1,12 @@
-#TODO
+from src.db_connection import get_connection
+
+def adicionar_leitor():
+    nome = input("Nome: ")
+    contato = input("Contato (e-mail/telefone): ")
+    
+    conexao = get_connection()
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO leitores (nome, contato) VALUES (?, ?)", (nome, contato))
+    conexao.commit()
+    conexao.close()
+    print("Leitor adicionado com sucesso!")
