389ab82e922be670301530017b3561fb7c122446
Eduardo Ribeiro <eduardorb2842@gmail.com>
2024-11-19 14:28:05 +0000

    Conexao base de dados

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,1 +1,14 @@
-#TODO
+import sqlite3
+
+def get_connection():
+    """
+    Retorna uma conexão com o banco de dados SQLite.
+    """
+    return sqlite3.connect('biblioteca.db')
+
+def inicializar_bd():
+    """
+    Inicializa o banco de dados criando todas as tabelas necessárias.
+    """
+    conexao = get_connection()
+    cursor = conexao.cursor()
