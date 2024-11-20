02cff8112e9fc0b9661e15809939408555128f60
Timur Sysoev <a85296@ualg.pt>
2024-11-20 16:41:03 +0000

    Adicionado a funçao atualizar_funcionarios

--- src/funcionarios.py
+++ src/funcionarios.py
@@ -23,3 +23,17 @@ def listar_funcionarios():
             print(f"ID: {funcionario[0]}, Nome: {funcionario[1]}, Cargo: {funcionario[2]}")
     else:
         print("Nenhum funcionário encontrado.")
+
+def atualizar_funcionario():
+    listar_funcionarios()
+    funcionario_id = int(input("ID do funcionário a ser atualizado: "))
+    novo_nome = input("Novo nome: ")
+    novo_cargo = input("Novo cargo: ")
+    
+    conexao = get_connection()
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE funcionarios SET nome = ?, cargo = ? WHERE id = ?", 
+                   (novo_nome, novo_cargo, funcionario_id))
+    conexao.commit()
+    conexao.close()
+    print("Funcionário atualizado com sucesso!")
