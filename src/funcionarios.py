4a2412bb15f08edba22bf070ca6b4670b331509c
Timur Sysoev <a85296@ualg.pt>
2024-11-20 16:21:30 +0000

    Adicionado a funçao listar_funcionarioes

--- src/funcionarios.py
+++ src/funcionarios.py
@@ -10,3 +10,16 @@ def adicionar_funcionario():
     conexao.commit()
     conexao.close()
     print("Funcionário adicionado com sucesso!")
+
+def listar_funcionarios():
+    conexao = get_connection()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM funcionarios")
+    funcionarios = cursor.fetchall()
+    conexao.close()
+    
+    if funcionarios:
+        for funcionario in funcionario:
+            print(f"ID: {funcionario[0]}, Nome: {funcionario[1]}, Cargo: {funcionario[2]}")
+    else:
+        print("Nenhum funcionário encontrado.")
