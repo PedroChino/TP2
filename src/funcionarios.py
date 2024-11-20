f4c92243a18b48783ed1474845cd1238cdd2b5e6
Timur Sysoev <a85296@ualg.pt>
2024-11-20 16:08:00 +0000

    adicionado a conexao da base de dados e a funçao adicionar_funcionarios

--- src/funcionarios.py
+++ src/funcionarios.py
@@ -1,1 +1,12 @@
-#TODO
+from src.db_connection import get_connection
+
+def adicionar_funcionario():
+    nome = input("Nome: ")
+    cargo = input("Cargo: ")
+    
+    conexao = get_connection()
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO funcionarios (nome, cargo) VALUES (?, ?)", (nome, cargo))
+    conexao.commit()
+    conexao.close()
+    print("Funcionário adicionado com sucesso!")
